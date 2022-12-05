904dab0c008b00d201b93581181da2446f69e69b
Jonathan Ming <jming422@gmail.com>
2022-12-04 22:41:10 -0800

    updates

--- doom/config.el
+++ doom/config.el
@@ -104,7 +104,16 @@
 ;; Package configuration
 (after! lsp-mode
   (setq lsp-rust-analyzer-proc-macro-enable t)
-  (setq lsp-metals-server-command "metals-emacs"))
+  ;; Add support for crystalline instead of scry
+  (add-to-list 'lsp-language-id-configuration '(crystal-mode . "crystal"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("crystalline"))
+                    :activation-fn (lsp-activate-on "crystal")
+                    :priority '1
+                    :server-id 'crystalline)))
+
+(add-hook! crystal-mode
+  (lsp))
 
 (after! dap-mode
   (dap-register-debug-template "Node::Attach" '(:type "node" :request "attach" :name "Node::Attach")))
