65e9cc815fe57528a25b99d140e36ec96aae99a4
Jonathan Ming <jming422@gmail.com>
2023-01-17 23:00:09 -0800

    Install logseq, update Doom theme

--- doom/config.el
+++ doom/config.el
@@ -27,7 +27,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function.
-(setq doom-theme 'doom-tomorrow-day)
+(setq doom-theme (if IS-MAC 'doom-tomorrow-day 'doom-miramare))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -50,7 +50,6 @@
 
 ;; Linux customizations
 (when IS-LINUX
-  (load-theme 'doom-palenight t)
   ;; To use Magit Forge without authinfo.gpg: 1) have a Secret Service API implementing service running, like gnome-keyring, and 2) run THIS elisp
   ;; (secrets-create-item "Login" "Emacs Forge GitHub Token" "<the-token>" :user "<github-username>^forge" :host "api.github.com")
   (setq auth-sources '("secrets:Login" "~/.config/emacs/.local/etc/authinfo.gpg" "~/.authinfo.gpg")))
